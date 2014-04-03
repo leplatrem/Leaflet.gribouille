9b0f3e5d4e228c257ee0069eeae8b08bc6619a33
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-04-03 15:20:11 +0200

    Pain polygons

--- index.js
+++ index.js
@@ -7,7 +7,21 @@
 
 
         map.on('mousemove', function (e) {
-            L.circle(e.latlng, 200).addTo(map);
+            var radius = map.layerPointToLatLng([0, 0]).lat -
+                         map.layerPointToLatLng([5, 5]).lat;
+            circle2polygon(e.latlng, radius, 32).addTo(map);
         });
     };
+
+
+    function circle2polygon(center, radius, points) {
+        var latlngs = [];
+        for (var i=0; i<points; i++) {
+            var radians = (i / points) * 2.0 * Math.PI;
+            var lat = center.lat + radius * Math.sin(radians),
+                lng = center.lng + radius * Math.cos(radians);
+            latlngs.push([lat, lng]);
+        }
+        return L.polygon(latlngs, {fillOpacity: 1.0});
+    }
 })();

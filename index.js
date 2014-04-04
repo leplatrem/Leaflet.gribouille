82b503b1d429c1b563cc25304e90c88fab22f10b
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-04-04 10:36:39 +0200

    Switch to exPolygons

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
         map.dragging.disable();
 
-        var polygon = L.multiPolygon([], {weight: 1,
+        var multipolygon = L.multiPolygon([], {weight: 1,
                                           fillOpacity: 0.5})
                        .addTo(map);
 
@@ -18,21 +18,24 @@
         map.on('mousedown mouseup', function () { enabled = !enabled; });
         map.on('mousemove', function (e) {
             if (!enabled) return;
+            var drawn = multipolygon.getLayers().map(function (polygon) {
+                return polygon.getLatLngs().concat(polygon._holes);
+            });
             var dot = circle2polygon(e.latlng, radius, RESOLUTION);
             // Merged multi-polygon
-            polygon .setLatLngs(mergePolygons([dot.getLatLngs()],
-                                              polygon.getLatLngs()));
+            var merged = mergePolygons(drawn, dot);
+            multipolygon.setLatLngs(merged);
         });
         map.on('zoomend', function () {
             // Adjust pen size on zoom
             radius = map.layerPointToLatLng([0, 0]).lat -
-                     map.layerPointToLatLng([0, SIZE]).lat;
+                     map.layerPointToLatLng([0, SIZE/2]).lat;
         });
 
         setInterval(function () {
             // Render GeoJSON in text area
             document.getElementById('geojson')
-                    .innerHTML = JSON.stringify(polygon.toGeoJSON());
+                    .innerHTML = JSON.stringify(multipolygon.toGeoJSON());
         }, 500);
     };
 
@@ -43,15 +46,16 @@
             var radians = (i / points) * 2.0 * Math.PI;
             var lat = center.lat + radius * Math.sin(radians),
                 lng = center.lng + radius * Math.cos(radians);
-            latlngs.push([lat, lng]);
+            latlngs.push(L.latLng(lat, lng));
         }
-        return L.polygon([latlngs]);
+        return [latlngs];
     }
 
 
     function mergePolygons(first, second) {
         var first_path = latLngs2Path(first),
             second_path = latLngs2Path(second);
+
         var scale = 10000;
         ClipperLib.JS.ScaleUpPaths(first_path, scale);
         ClipperLib.JS.ScaleUpPaths(second_path, scale);
@@ -59,30 +63,48 @@
         cpr.AddPaths(first_path, ClipperLib.PolyType.ptSubject, true);
         cpr.AddPaths(second_path, ClipperLib.PolyType.ptClip, true);
         var fillType = ClipperLib.PolyFillType.pftNonZero;
-        var result = new ClipperLib.Paths();
+        var result = new ClipperLib.PolyTree();
         cpr.Execute(ClipperLib.ClipType.ctUnion, result, fillType, fillType);
-        return path2LatLngs(result);
+        var polygonsholes = new ClipperLib.ExPolygons();
+        polygonsholes = ClipperLib.JS.PolyTreeToExPolygons(result, polygonsholes);
+        return path2LatLngs(polygonsholes);
 
-        function latLngs2Path(latlngs) {
-            return convert(latlngs, true);
-        }
-        function path2LatLngs(path) {
-            return convert(path, false);
-        }
-        function convert(coords, from) {
+        function latLngs2Path(coords) {
             var paths = [];
             for (var i=0; i<coords.length; i++) {
                 var poly = coords[i],
                     subpath = [];
                 for (var j=0; j<poly.length; j++) {
                     var latlng = poly[j],
-                        coord = from ? {X: latlng.lng, Y: latlng.lat} :
-                                       [latlng.Y / scale, latlng.X / scale];
+                        coord = {X: latlng.lng, Y: latlng.lat};
                     subpath.push(coord);
                 }
                 paths.push(subpath);
             }
             return paths;
+        }
+        function path2LatLngs(expolygons) {
+            var multipolygons = [];
+            for (var i=0; i<expolygons.length; i++) {
+                var expolygon = expolygons[i];
+                var outer = [];
+                for (var j=0; j<expolygon.outer.length; j++) {
+                    var latlng = expolygon.outer[j];
+                    outer.push([latlng.Y / scale, latlng.X / scale]);
+                }
+                var holes = [];
+                for (var j=0; j<expolygon.holes.length; j++) {
+                    var hole = expolygon.holes[j];
+                    holes[j] = [];
+                    for (var k=0; k<hole.length; k++) {
+                        var latlng = hole[k];
+                        holes[j].push([latlng.Y / scale, latlng.X / scale]);
+                    }
+                }
+                var polygon = ([outer]).concat(holes);
+                multipolygons.push(polygon);
+            }
+            return multipolygons;
         }
     }
 })();
